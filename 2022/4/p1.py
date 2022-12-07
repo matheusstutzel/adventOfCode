226a3cd431adf4eb15294024dbe40967a14a1a7f
Matheus Stutzel <matheusstutzel@yahoo.com.br>
2022-12-06 22:17:44 -0300

    2022/4

---
+++ 2022/4/p1.py
@@ -0,0 +1,24 @@
+import sys
+
+t = 0
+
+# reading input
+for line in sys.stdin:
+    l = line.rstrip()
+    a,b = l.split(",")
+    a1,a2 = a.split("-")
+    b1,b2 = b.split("-")
+
+    a1 = int(a1)
+    a2 = int(a2)
+    b1 = int(b1)
+    b2 = int(b2)
+    
+    #make sure a<b
+    if((a2-a1)>(b2-b1)):
+        a1,a2,b1,b2=b1,b2,a1,a2
+
+    if(a1>=b1 and a2<=b2):
+        t = t+1
+
+print(t)
