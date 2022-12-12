4d411e94268ccd9f2bb93fa5e160c8df0746f6c2
matheusstutzel <matheusstutzel@yahoo.com.br>
2022-12-12 01:39:00 +0000

    2022/10 clean

--- 2022/10/p1.py
+++ 2022/10/p1.py
@@ -1,25 +1,23 @@
 import sys
+
+prints = False
+
 size = 300
 cycle = [1]*size
-
-
-input = [i.rstrip() for i in sys.stdin]
-
 i = 1
 
-for l in input:
-    if(l[0] == 'n'):
+for l in [i.rstrip() for i in sys.stdin]:
+    i=i+1
+    cycle[i]=cycle[i-1]
+    if(l[0] == 'a'):
         i=i+1
-        cycle[i]=cycle[i-1]
-    else:
-        i=i+2
-        cycle[i-1] = cycle[i-2]
         cycle[i] = cycle[i-1] + int(l.split(" ")[1])
-    print(l.split(" ")[0], cycle[max(0,i-5):i+1], i )
+    if prints:
+        print(l.split(" ")[0], cycle[max(0,i-5):i+1], i )
+
 for t in range(i+1, size):
     cycle[t]=cycle[t-1]
 
-print(cycle[:21])
 r = 0
 for i in range(6):
     r = r + cycle[20+ 40*i]*(20+40*i)
--- 2022/10/p2.py
+++ 2022/10/p2.py
@@ -1,40 +1,38 @@
 import sys
 
+prints = False
+
 def printa (x, pos, r):
     print("Sprint: ", end="")
     for i in range(40):
-        print("#" if i>= x-1 and i<=x+1 else ".", end="")
-    print("\nstarting cycle", pos, " X = ", x, "will print: ", "#" if r else "." )
+        print("🟥" if i>= x-1 and i<=x+1 else "⬛️", end="")
+    print("\nstarting cycle", pos, " X = ", x, "will print: ", "🟥" if r else "⬛️" )
     print("\n")
 
 
 def test(x, pos):
     pos = (pos%40)
     r= pos >=(x-1) and pos<=(x+1)
-    #printa(x, pos, r)
+    if prints:
+        printa(x, pos, r)
     return r
+
+
 size = 300
 cycle = [1]*size
-
-
-input = [i.rstrip() for i in sys.stdin]
-
 i = 1
 
-for l in input:
-    if(l[0] == 'n'):
+for l in [i.rstrip() for i in sys.stdin]:
+    i=i+1
+    cycle[i]=cycle[i-1]
+    if(l[0] == 'a'):
         i=i+1
-        cycle[i]=cycle[i-1]
-    else:
-        i=i+2
-        cycle[i-1] = cycle[i-2]
         cycle[i] = cycle[i-1] + int(l.split(" ")[1])
-    #print(l.split(" ")[0], cycle[max(0,i-5):i+1], i )
+    if prints:
+        print(l.split(" ")[0], cycle[max(0,i-5):i+1], i )
+
 for t in range(i+1, size):
     cycle[t]=cycle[t-1]
-
-print(cycle[:21])
-
 
 resp = ""
 for i in range(6):
