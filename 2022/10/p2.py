c22f2020e4fac164c17967e282cee24dea945c33
matheusstutzel <matheusstutzel@yahoo.com.br>
2022-12-12 01:33:15 +0000

    2022/10

--- 2022/10/p2.py
+++ 2022/10/p2.py
@@ -4,10 +4,12 @@ def printa (x, pos, r):
     print("Sprint: ", end="")
     for i in range(40):
         print("#" if i>= x-1 and i<=x+1 else ".", end="")
-    print("\nstarting cycle", pos, " will print: ", "#" if r else "." )
+    print("\nstarting cycle", pos, " X = ", x, "will print: ", "#" if r else "." )
+    print("\n")
 
 
 def test(x, pos):
+    pos = (pos%40)
     r= pos >=(x-1) and pos<=(x+1)
     #printa(x, pos, r)
     return r
@@ -27,15 +29,17 @@ for l in input:
         i=i+2
         cycle[i-1] = cycle[i-2]
         cycle[i] = cycle[i-1] + int(l.split(" ")[1])
-    print(l.split(" ")[0], cycle[max(0,i-5):i+1], i )
+    #print(l.split(" ")[0], cycle[max(0,i-5):i+1], i )
 for t in range(i+1, size):
     cycle[t]=cycle[t-1]
 
 print(cycle[:21])
-r = [["."]*40]*6
 
+
+resp = ""
 for i in range(6):
     for j in range(40):
-        pos = 1+ i*40 + j
-        print( "#" if test(cycle[pos],pos) else '.', end="")
-    print(" ")
+        pos = i*40 + j
+        resp = resp + ( "🟥" if test(cycle[pos+1],pos) else "⬛️")
+    resp= resp+ "\n"
+print(resp)
