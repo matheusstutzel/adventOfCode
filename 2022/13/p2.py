81a6d9b76639f152c4de69aa64ecd8baf763dd1b
Matheus Stutzel <matheusstutzel@yahoo.com.br>
2022-12-13 15:03:05 -0300

    2022/13

---
+++ 2022/13/p2.py
@@ -0,0 +1,50 @@
+import sys
+import json
+from functools import cmp_to_key
+from math import  prod
+
+
+#a<b -1
+#a==b 0
+#a>b  1
+def compareInt(a,b, pre):
+    print(pre, "Compare", a,b)
+    if(a==b):
+        return 0 
+    return 1 if a>b else -1
+def compare(a,b, pre = ""):
+    print(pre, "Compare", a,b)
+    i = 0
+    while i<len(a) and i<len(b):
+        x = a[i]
+        y = b[i]
+        comp = 0
+        if(isinstance(x,int) and isinstance(y, int)):
+            comp = compareInt(x,y, pre)
+        else:
+            if(isinstance(x, int)):
+                x = [x]
+            if(isinstance(y, int)):
+                y = [y]
+            comp = compare(x,y, pre+"  ")
+        if comp!=0:
+            return comp
+        i = i+1
+
+    if i==len(a) and i == len(b):
+        return 0            
+    elif(i==len(a)):
+        print("left side is smaller")
+        return -1
+    else:
+        print("right side is smaller")
+        return 1
+    
+
+
+l = [json.loads(x.strip()) for x in sys.stdin if(x!="\n")]
+l.append([[2]])
+l.append([[6]])
+
+l = sorted(l, key = cmp_to_key(compare))
+print(prod([idx+1 for idx,v in enumerate(l) if v==[[2]] or v == [[6]]]))
