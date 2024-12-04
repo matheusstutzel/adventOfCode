be28bce4ef48ea5e747cf9f2efe3f72b0b340417
Matheus Stutzel <matheusstutzel@yahoo.com.br>
2024-12-04 03:19:15 +0000

    2024/03 elixir

---
+++ 2024/03/p1.exs
@@ -0,0 +1,14 @@
+defmodule P03 do
+	def calc(input)do
+		Regex.scan(~r/mul\(([0-9]{1,3}),([0-9]{1,3})\)/, input, [])
+		#|> IO.inspect
+		|> Enum.map(fn [_,a,b]->String.to_integer(a)*String.to_integer(b)end)
+		#|> IO.inspect
+		|> Enum.sum
+		
+	end
+end
+
+IO.read(:stdio, :eof)
+|> P03.calc
+|> IO.inspect
