01f7bf1dd78991da67678d07698f7f90206ec0ce
Matheus Stutzel <matheusstutzel@yahoo.com.br>
2024-12-09 09:40:29 +0000

    2024/07 p2

---
+++ 2024/07/p2.py
@@ -0,0 +1,25 @@
+import sys
+
+def test(k,v):
+    if v[0]>k:
+        return False
+    if len(v)==1:
+        return v[0]==k
+    if(test(k, [v[0]+v[1]]+v[2:])):
+        return True
+    if(test(k, [v[0]*v[1]]+v[2:])):
+        return True
+    if(test(k, [int(str(v[0])+str(v[1]))]+v[2:])):
+        return True
+    return False
+        
+
+sum=0
+
+for l in sys.stdin:
+    expected,rest=l.rstrip().split(":")
+    v=[int(x.strip()) for x in rest.strip().split()]
+    k=int(expected)
+    if(test(k,v)):
+        sum+=k
+print(sum)
