52a0041cd922fd530542eb4342d184e400d81f52
Matheus Stutzel <matheusstutzel@yahoo.com.br>
2024-12-21 21:07:48 +0000

    2024/21 p1

---
+++ 2024/21/p1.py
@@ -0,0 +1,127 @@
+import sys
+from collections import deque
+import math
+
+direction = {
+(0,1):'>',
+(-1,0):'^',
+(0,-1):'<',
+(1,0):"v"
+}
+
+
+def valid(mat, x,y):
+    return x>=0 and x<len(mat) and y>=0 and y<len(mat[x]) and mat[x][y]!='#'
+
+def precalc(mat):
+    resp = {}
+    for i in range(len(mat)):
+        for j in range(len(mat[i])):
+            if mat[i][j]=='#':
+                continue
+            for k in range(len(mat)):
+                for l in range(len(mat[k])):
+                    if k == i and l==j:
+                        continue
+                    resp[(i,j,k,l)] = paths(mat,(i,j),(k,l))
+    return resp    
+    
+def paths(mat, start, end):
+    q = deque([(*start, [start])])
+    result = []
+    m = math.inf
+    while q:
+        x,y,path = q.popleft()
+        if (x,y) == end and len(path)<=m:
+            if len(path) < m:
+                result.clear()
+            m = len(path)
+            result.append(path)
+            continue
+        for d in direction:
+            u = (x+d[0], y+d[1])
+            if valid(mat, *u) and u not in path:
+                q.append((*u, path+[u]))  
+    final=[]
+    for r in result:
+        f =[]
+        for i in range(1,len(r)):
+            mov = (r[i][0]-r[i-1][0], r[i][1]-r[i-1][1])   
+            #print(r, r[i-1], r[i]) 
+            f.append(direction[mov])
+        f.append('A')
+        final.append(f)
+    return final
+
+def toCord(mat):
+    result = {}
+    for i in range(len(mat)):
+        for j in range(len(mat[i])):
+            result[mat[i][j]]=(i,j)
+    return result
+
+
+pad = [
+    "789",
+    "456",
+    "123",
+    "#0A"
+]
+pad2cord = toCord(pad)
+
+padpaths = precalc(pad) #precalc
+
+dpad = [
+    "#^A",
+    "<v>"
+]
+dpad2cord = toCord(dpad)
+dpadpaths = precalc(dpad) #precalc
+
+
+def genericpad(c,p2cord, allpaths, mat, start = 'A'):
+    current = p2cord[start]
+    options = []
+    for i in c:
+        target = p2cord[i]
+        
+        paths = allpaths[(*current, *target)] if current!=target else [['A']]
+        #print("going from ", current, "(",mat[current[0]][current[1]],") to ", target, "(",mat[target[0]][target[1]],") path ", paths)
+        response = []
+        if len(options) ==0:
+            response = []+paths
+        else:
+            for o in options:
+                for p in paths:
+                    response.append(o+p)
+        options = response        
+        current = target
+    m = min([len(o) for o in options])
+    return ["".join(o) for o in options if len(o)==m]
+def numpad(c):
+    #print("numapd",c)
+    return genericpad(c, pad2cord, padpaths, pad)
+def dirpad(c):
+    r = []
+    for x in c:
+        #print("dirpad", x)
+        r.extend(genericpad(x, dpad2cord, dpadpaths, dpad))
+    m = min(len(x) for x in r )
+    return [x for x in r if len(x)==m]
+def solve(c):
+    options = dirpad(dirpad(numpad(c)))
+    #print(options)
+    return min([len(x) for x in options])
+
+
+
+l = [l.strip() for l in sys.stdin]
+
+resp = 0
+for c in l:
+    numeric = int(c.replace('A', ''))
+    size = solve(c)
+    print(size, numeric)
+    resp+= size*numeric
+print(resp)
+    
